9169bb337a01a08bf9c589a56d3ff482370d44d8
Gongtup Yeepacha <jobreak11@gmail.com>
2026-08-26 12:28:05 +0700

    added_'O'_on_tic_tac_toe

--- src/dockers/nextjs/nextjs/app/tic-tac-toe/page.tsx
+++ src/dockers/nextjs/nextjs/app/tic-tac-toe/page.tsx
@@ -3,40 +3,50 @@
 import { useState } from 'react';
 import Link from "next/link"
 
-
-interface SquareProps{
-  value: number | string | null;
+interface SquareProps {
+  square: number | string | null;
+  onSquareClick: () => void; 
 }
 
-function Square() {
-  const [value, setValue] = useState<string | null>(null);
-  function handleClick(){
-    setValue('X');
-  }
+
+function Square({ square, onSquareClick }: SquareProps) {
   return <button className="w-12 h-12 bg-white text-black border border-gray-400 font-bold text-xl m-[2px] hover:bg-gray-100 flex items-center justify-center"
-          onClick={handleClick}
+          onClick={onSquareClick}
           >
-          {value}
+          {square}
           </button>;
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i:number){
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else{
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <div className="flex flex-col items-center mt-10">
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square square={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square square={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square square={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square square={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square square={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square square={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="flex">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square square={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square square={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square square={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </div>
   );
