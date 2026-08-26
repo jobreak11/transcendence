7abe4bcd1f2efe4083711f8705cfd8cb74da2c0d
Gongtup Yeepacha <jobreak11@gmail.com>
2026-08-26 16:49:06 +0700

    added_history_feature_on_tic_tac_toe

--- src/dockers/nextjs/nextjs/app/tic-tac-toe/page.tsx
+++ src/dockers/nextjs/nextjs/app/tic-tac-toe/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Link from "next/link"
+import { div, li } from 'motion/react-client';
 
 interface SquareProps {
   square: number | string | null;
@@ -17,19 +18,9 @@ function Square({ square, onSquareClick }: SquareProps) {
           </button>;
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const winner: number | string | null = calculateWinner(squares);
-  let status:string;
-  if (winner){
-    status = "Winner: " + winner;
-  }
-  else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i:number){
-    if (squares[i]){
+    if (calculateWinner(squares) || squares[i]){
       return ;
     }
     const nextSquares = squares.slice();
@@ -39,8 +30,15 @@ export default function Board() {
     else{
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+  }
+  const winner: number | string | null = calculateWinner(squares);
+  let status:string;
+  if (winner){
+    status = "Winner: " + winner;
+  }
+  else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
   return (
     <div className="flex flex-col items-center mt-10">
@@ -84,3 +82,41 @@ function calculateWinner(squares){
   return null;
 }
 
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+  function jumpTo(nextMove) {
+      setCurrentMove(nextMove);
+  }
+  const moves = history.map((squares:number[][], move) => {
+    let description:string;
+    if (move > 0){
+      description = 'Go to move #' + move;
+    }
+    else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
