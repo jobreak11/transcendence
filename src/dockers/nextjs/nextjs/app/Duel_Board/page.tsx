d57d6973a5ac99577bda9916c9859c55c0345d95
Gongtup Yeepacha <jobreak11@gmail.com>
2026-09-03 14:40:43 +0700

    adjust_board_related_on_poker_board

--- src/dockers/nextjs/nextjs/app/Duel_Board/page.tsx
+++ src/dockers/nextjs/nextjs/app/Duel_Board/page.tsx
@@ -19,13 +19,6 @@ let currentPhase: GamePhase = "Main_Phase"
 
 function BoardSlot() {
 	return (
-		// <div className='absolute bottom-10 w-full flex justify-center gap-6 px-4'>
-		// 	<div className="w-[100px] h-[140px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg"></div>
-		// 	<div className="w-[100px] h-[140px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg"></div>
-		// 	<div className="w-[100px] h-[140px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg"></div>
-		// 	<div className="w-[100px] h-[140px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg"></div>
-		// 	<div className="w-[100px] h-[140px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg"></div>
-		// </div>
 		<div className='w-[90px] h-[120px] bg-slate-900 border-2 border-dashed border-slate-500 rounded-lg hover:border-red-400 transition-all cursor-pointer shadow-lg'>
         </div>		
 	)
@@ -35,26 +28,28 @@ export default function Page() {
 	return (
 		<div className='bg-black min-h-screen text-white flex flex-col items-center justify-center p-10'>
 			<h1 className='text-4xl font-bold mb-6'>Duel Board</h1>
-			<p className="mb-8">เตรียมพร้อมเข้าสู่สนามประลอง 2D!</p>
+			<div className='flex justify-center gap-4'>
+				<BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot />
+			</div>
 			{/* ปุ่มกดกลับไปหน้าหลัก*/}
 			<Link href='../' className='bg-slate-700 p-3 rounded-xl hover:border-2 hover:border-white transition-all'>
 					Back to Home
 			</Link>
 			{/* <BoardSlot /> */}
 			<div className='absolute bottom-10 w-full flex justify-center gap-4'>
-				<BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot />
+				<BoardSlot /><BoardSlot />
 			</div>
 
 			<div className='absolute top-10 w-full flex justify-center gap-4'>
-				<BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot />
+				<BoardSlot /><BoardSlot />
 			</div>
 
 			<div className='absolute left-10 h-full flex flex-col justify-center gap-4'>
-				<BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot />
+				<BoardSlot /><BoardSlot />
 			</div>
 
 			<div className='absolute right-10 h-full flex flex-col justify-center gap-4'>
-				<BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot /><BoardSlot />
+				<BoardSlot /><BoardSlot />
 			</div>
 		</div>
 	)
